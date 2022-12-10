ed950c3d40a366f0b544c3642c94ef1ed2fb3320
ruben-hutter <hutter.ruben@gmail.com>
2022-12-10 16:58:12 +0100

    second part done

--- 9/main.py
+++ 9/main.py
@@ -1,11 +1,11 @@
-with open("9/test_input2", 'r') as f:
+with open("9/input", 'r') as f:
     lines = f.read().splitlines()
 
 moves = [(line[0], int(line[2:])) for line in lines] # [('R', 12), ...]
-head_position = [0, 0]
 
 def first_part():
     visited = set()
+    head_position = [0, 0]
     tail_position = [0, 0]
     for move in moves:
         direction, value = move[0], move[1]
@@ -45,42 +45,46 @@ def check_tail_update(head_position, tail_position):
 
 def second_part():
     visited = set()
-    tail_position = [[0, 0] for _ in range(9)] # 9-8-7-6-5-4-3-2-1-H
+    number_of_knots = 10
+    rope_knots_position = [[0, 0] for _ in range(number_of_knots)] # 9-8-7-6-5-4-3-2-1-H
     for move in moves:
         direction, value = move[0], move[1]
         # move head
         while value > 0:
             # check in which direction
             if direction == 'L': # move left
-                head_position[0] -= 1
+                rope_knots_position[0][0] -= 1
             elif direction == 'R': # move right
-                head_position[0] += 1
+                rope_knots_position[0][0] += 1
             elif direction == 'U': # move up
-                head_position[1] += 1
+                rope_knots_position[0][1] += 1
             elif direction == 'D': # move down
-                head_position[1] -= 1
+                rope_knots_position[0][1] -= 1
             # check if tail has to move
-            check_tail_update2(head_position, tail_position)
-            visited.add(tuple(tail_position[-1]))
+            update_tail(rope_knots_position, 1)
+            visited.add(tuple(rope_knots_position[-1]))
             # decrement value
             value -= 1
     print(f'Visited positions: {len(visited)}')
 
-def check_tail_update2(head_position, tail_position):
-    x_diff = head_position[0] - tail_position[0]
-    y_diff = head_position[1] - tail_position[1]
+def update_tail(rope_knots_position, tail_index):
+    if tail_index == len(rope_knots_position):
+        return
+    x_diff = rope_knots_position[tail_index-1][0] - rope_knots_position[tail_index][0]
+    y_diff = rope_knots_position[tail_index-1][1] - rope_knots_position[tail_index][1]
     # no need to update tail
     if abs(x_diff) < 2 and abs(y_diff) < 2:
         return
     # change tail position
     if x_diff < 0: # H.T -> HT.
-        tail_position[0] -= 1
+        rope_knots_position[tail_index][0] -= 1
     elif x_diff > 0: # T.H -> .TH
-        tail_position[0] += 1
+        rope_knots_position[tail_index][0] += 1
     if y_diff < 0: # head under tail -> move tail down
-        tail_position[1] -= 1
+        rope_knots_position[tail_index][1] -= 1
     elif y_diff > 0: # tail under head -> move tail up
-        tail_position[1] += 1
+        rope_knots_position[tail_index][1] += 1
+    update_tail(rope_knots_position, tail_index+1)
 
 def main():
     first_part()
