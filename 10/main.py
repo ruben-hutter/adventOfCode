fcf307dcfeadad839e198acaaa9773e9172dd553
ruben-hutter <hutter.ruben@gmail.com>
2022-12-10 21:36:40 +0100

    day 10 - second part

--- 10/main.py
+++ 10/main.py
@@ -1,6 +1,9 @@
 with open("10/input", 'r') as f:
     lines = f.read().splitlines()
 
+CRT = [['.' for _ in range(40)] for _ in range(6)]
+DRAW = '#'
+
 def first_part():
     signal_strength = 0
     cycle = 0
@@ -22,7 +25,33 @@ def first_part():
     print(f'Signal strength: {signal_strength}')
 
 def second_part():
-    return
+    cycle = 0
+    reg_x = 1
+    for instruction in lines:
+        cmd = instruction[:4]
+        if cmd == 'addx':
+            cycle += 1
+            # check if pixel is drawn
+            if (cycle-1)%40 in [reg_x-1, reg_x, reg_x+1]:
+                CRT[(cycle-1)//40][(cycle-1)%40] = DRAW
+            cycle += 1
+            # check if pixel is drawn
+            if (cycle-1)%40 in [reg_x-1, reg_x, reg_x+1]:
+                CRT[(cycle-1)//40][(cycle-1)%40] = DRAW
+            reg_x += int(instruction[4:]) # increse register
+        else:
+            cycle += 1
+            # check if pixel is drawn
+            if (cycle-1)%40 in [reg_x-1, reg_x, reg_x+1]:
+                CRT[(cycle-1)//40][(cycle-1)%40] = DRAW
+    print_crt(CRT)
+
+def print_crt(crt):
+    print('CRT result:')
+    print(' ' + '-' * 40 + ' ')
+    for line in crt:
+        print('|' + ''.join(line) + '|')
+    print(' ' + '-' * 40 + ' ')
 
 def main():
     first_part()
