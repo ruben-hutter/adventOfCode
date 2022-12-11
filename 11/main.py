8b703466968b3cfe5907997c9e335817de15b1a6
ruben-hutter <hutter.ruben@gmail.com>
2022-12-11 12:43:01 +0100

    day 11 - elaborate input

--- 11/main.py
+++ 11/main.py
@@ -1,3 +1,21 @@
+with open("11/input", 'r') as f:
+    monkeys = f.read()
+
+monkeys = [monkey for monkey in monkeys.split('\n\n')]
+monkeys = [monkey.split('\n') for monkey in monkeys]
+monkeys = [[elem.strip() for elem in monkey] for monkey in monkeys]
+
+# [Monkey 0: [[54, 89, 94], 'old * 7', 17, 5, 3], ...]
+for monkey in monkeys:
+    items = monkey[1][16:].split(', ')
+    operation = lambda old, op=monkey[2][17:]: eval(op)
+    test = int(monkey[3].rsplit(maxsplit=1)[-1])
+    if_true = int(monkey[4].rsplit(maxsplit=1)[-1])
+    if_false = int(monkey[5].rsplit(maxsplit=1)[-1])
+
+    # define monkey as list of the above defined elements
+    monkey = [items, operation, test, if_true, if_false]
+
 def first_part():
     return
 
