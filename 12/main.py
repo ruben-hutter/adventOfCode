abfd53224099dcc2ddd6eece67d1fc07d4b4cc5f
ruben-hutter <hutter.ruben@gmail.com>
2022-12-29 13:56:25 +0100

    day 12 - solved

--- 12/main.py
+++ 12/main.py
@@ -1,27 +1,57 @@
+import sys
 from collections import deque
 
 graph = {}
+starting_nodes = []
+in_width = 0
+in_height = 0
 
-with open("12/test_input", 'r') as f:
+def calc_step(letter):
+    if letter == 'S':
+        return 0
+    elif letter == 'E':
+        return 26
+    return ord(letter) - ord('a')
+
+def append_neighbors(row, col):
+    if col > 0 and (row,col-1) in graph: # has left neighbor
+        difference = graph[(row,col-1)][0] - graph[(row,col)][0]
+        if difference <= 1:
+            graph[(row,col)][1].append((row,col-1))
+    if col < in_width-1: # has right neighbor
+        difference = calc_step(in_file[row][col+1]) - graph[(row,col)][0]
+        if difference <= 1:
+            graph[(row,col)][1].append((row,col+1))
+    if row > 0 and (row-1,col) in graph: # has upper neighbor
+        difference = graph[(row-1,col)][0] - graph[(row,col)][0]
+        if difference <= 1:
+            graph[(row,col)][1].append((row-1,col))
+    if row < in_height-1: # has lower neighbor
+        difference = calc_step(in_file[row+1][col]) - graph[(row,col)][0]
+        if difference <= 1:
+            graph[(row,col)][1].append((row+1,col))
+
+with open("12/input", 'r') as f:
     in_file = f.read().splitlines()
+    in_height = len(in_file)
+    in_width = len(in_file[0])
     for row, line in enumerate(in_file):
         for col, letter in enumerate(line):
+            # starting_nodes
             if letter == 'S':
-                # start_node
-                start_node = (row,col)
-                graph[(row,col)] = (0, [])
+                starting_nodes.insert(0, (row,col))
+            elif letter == 'a':
+                starting_nodes.append((row,col))
             elif letter == 'E':
                 # end_node
                 end_node = (row,col)
-                graph[(row,col)] = (26, [])
-            else:
-                graph[(row,col)] = (ord(letter)-ord('a'), [])
+            graph[(row,col)] = (calc_step(letter), [])
             # append neighbors
-
-print(graph)
+            append_neighbors(row, col)
 
 def first_part():
-    return
+    path = shortest_path(graph, starting_nodes[0], end_node)
+    print(f'Shortest path: {len(path)} steps')
 
 def bfs_with_predecessors(graph, node):
     predecessor = {}
@@ -52,7 +82,14 @@ def shortest_path(graph, from_node, to_node):
     return path
 
 def second_part():
-    return
+    shortest_size = sys.maxsize
+    shortest = 0
+    for start_node in starting_nodes:
+        path = shortest_path(graph, start_node, end_node)
+        if path and len(path) < shortest_size:
+            shortest = path
+            shortest_size = len(shortest)
+    print(f'Shortest path: {shortest_size} steps')
 
 def main():
     first_part()
