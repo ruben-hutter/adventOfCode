92831264d38c5eed6c5f9acc191955e50dfc8467
ruben-hutter <hutter.ruben@gmail.com>
2022-12-30 21:53:19 +0100

    day 13 - part 2 and reformatted file

--- 13/main.py
+++ 13/main.py
@@ -1,6 +1,7 @@
-signals = [] # contains tuples of packets
+from functools import cmp_to_key
 
 def first_part():
+    signals = [] # contains tuples of packets
     with open("13/input", 'r') as f:
         in_packet1, in_packet2, _ = [f.readline().strip() for _ in range(3)]
         while in_packet1:
@@ -9,7 +10,7 @@ def first_part():
     res = 0
     for idx, signal in enumerate(signals):
         packet1, packet2 = signal
-        if compare(packet1, packet2) == 1:
+        if compare(packet1, packet2) == -1:
             res += idx+1
     print(f'Total: {res}')
 
@@ -25,9 +26,9 @@ def compare(packet1, packet2):
         return _compare(packet1, packet2)
     # both are values
     if packet1 < packet2:
-        return 1
-    elif packet2 < packet1:
         return -1
+    elif packet2 < packet1:
+        return 1
     return 0
 
 def _compare(packet1, packet2):
@@ -35,23 +36,23 @@ def _compare(packet1, packet2):
     l_len, r_len = len(packet1), len(packet2)
     while idx < l_len and idx < r_len:
         res = compare(packet1[idx], packet2[idx])
-        if res == 1:
-            return 1
-        elif res == -1:
+        if res == -1:
             return -1
+        elif res == 1:
+            return 1
         idx += 1
     if idx == l_len:
         if l_len == r_len:
             return 0
-        return 1
-    return -1
+        return -1
+    return 1
 
 def second_part():
+    signals = [[[2]], [[6]]] # packets
     with open("13/input", 'r') as f:
-        in_packets = f.readline().strip()
-        while in_packet1:
-            signals.append((map(eval, [in_packet1, in_packet2])))
-            in_packet1, in_packet2, _ = [f.readline().strip() for _ in range(3)]
+        signals.extend([eval(packet) for packet in f.read().splitlines() if packet])
+    sorted_signals = sorted(signals, key=cmp_to_key(compare))
+    print(f'Result: {(sorted_signals.index([[2]])+1) * (sorted_signals.index([[6]])+1)}')
 
 def main():
     first_part()
