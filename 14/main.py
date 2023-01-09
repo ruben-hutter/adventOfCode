2a9e72e294e177687ca04609220033247a57d8d9
ruben-hutter <hutter.ruben@gmail.com>
2023-01-09 22:34:48 +0100

    day 14 - changed approach: refactored rock setup

--- 14/main.py
+++ 14/main.py
@@ -1,22 +1,34 @@
-from enum import Enum
+import sys
+
+# define coordinate type
+Coordinate = tuple[int, int]
 
 with open("14/test_input", 'r') as f:
     in_file = f.read().splitlines()
     in_file = [[eval(c) for c in l.split(" -> ")] for l in in_file]
 
 POURING_POINT = (500, 0)
-class Type(Enum):
-    ROCK = 1
-    AIR = 2
-    SAND = 3
 
-coordinates = {}
+coordinates = set()
 
 def first_part():
-    # initial setup from input
+    # initial rock setup
     for line in in_file:
-        for coord in line:
-            coordinates[coord] = Type.ROCK
+        for i in range(len(line)-1):
+            from_x, from_y = line[i]
+            to_x, to_y = line[i+1]
+            if from_x == to_x: # vertical line
+                for y in range(min(from_y,to_y), max(from_y,to_y)+1):
+                    coordinates.add((from_x,y))
+            else:
+                for x in range(min(from_x,to_x), max(from_x,to_x)+1):
+                    coordinates.add((x,from_y))
+    # count sand units
+    count = 0
+    print(f'Sand units: {count}')
+
+def update_coord():
+    return
 
 def second_part():
     return
