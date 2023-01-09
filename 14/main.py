73b62a81d6fc7c4283a4ea04e1b588b8b2d46246
ruben-hutter <hutter.ruben@gmail.com>
2023-01-09 16:36:12 +0100

    day 14 - read file and initial coordinate setup

--- 14/main.py
+++ 14/main.py
@@ -1,5 +1,22 @@
+from enum import Enum
+
+with open("14/test_input", 'r') as f:
+    in_file = f.read().splitlines()
+    in_file = [[eval(c) for c in l.split(" -> ")] for l in in_file]
+
+POURING_POINT = (500, 0)
+class Type(Enum):
+    ROCK = 1
+    AIR = 2
+    SAND = 3
+
+coordinates = {}
+
 def first_part():
-    return
+    # initial setup from input
+    for line in in_file:
+        for coord in line:
+            coordinates[coord] = Type.ROCK
 
 def second_part():
     return
