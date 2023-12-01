f27e367dad0c498aa32d35567ff4abb61fe7af2a
ruben-hutter <hutter.ruben@gmail.com>
2023-12-01 09:50:18 +0100

    Add part 1 day-01

--- 2023/day-01/src/bin/part1.rs
+++ 2023/day-01/src/bin/part1.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("part 1!");
+    let input = include_str!("./input.txt");
+    println!("{}", part1(input));
+}
+
+fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| {
+            let mut nums_iter = line.chars().filter(|c| c.is_ascii_digit());
+            let first = nums_iter.next().unwrap();
+            let last = nums_iter.last().unwrap_or(first);
+            let res = format!("{}{}", first, last).parse::<u32>().unwrap();
+            res
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(142, part1(input));
+    }
 }
